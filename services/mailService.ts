f52c60bbde610aa98d4fa4579bc47f5d2abe97f9
Shubham Chadokar <shubhamchadokar36@gmail.com>
2025-01-22 23:23:23 +0530

    created the mail service

---
+++ services/mailService.ts
@@ -0,0 +1,26 @@
+import fs from "fs";
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  secure: true,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+const sendMail = async ({ to, subject, html }) =>
+  await new Promise((resolve, reject) => {
+    const from = process.env.COMPANY + " <" + process.env.DISPLAY_EMAIL + ">";
+    const options = { from, to, subject, html };
+    transporter.sendMail(options, (err, info) => {
+      if (err) reject(err);
+      else resolve(info.response);
+    });
+  });
+
+const templateToHTML = (path: string) =>
+  fs.readFileSync(path, "utf-8").toString();
+
+module.exports = { sendMail, templateToHTML };
---
+++ services/misc.ts
@@ -0,0 +1,34 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const generateJWT = (
+  payload: object,
+  options: object = { expiresIn: "1d" }
+): string => {
+  return jwt.sign(payload, process.env.JWT_SECRET_KEY as string, options);
+};
+
+const parseToken = (req: any): string => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    throw new Error("Token not found");
+  }
+  return token;
+};
+
+const verifyJWT = (token: string): JwtPayload | string => {
+  return jwt.verify(token, process.env.JWT_SECRET_KEY as string);
+};
+
+const generateHash = async (
+  text: string,
+  saltRounds: number
+): Promise<string> => {
+  return await bcrypt.hash(String(text), saltRounds);
+};
+
+const compareHash = async (text: string, hash: string): Promise<boolean> => {
+  return await bcrypt.compare(String(text), hash);
+};
+
+export { generateHash, generateJWT, verifyJWT, parseToken, compareHash };
