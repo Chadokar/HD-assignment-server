269525a9ef384285c2de14135b5cd71bb08cdef0
Shubham Chadokar <shubhamchadokar36@gmail.com>
2025-01-22 22:09:26 +0530

    project started

---
+++ server.ts
@@ -0,0 +1,35 @@
+import express from "express";
+// import authRoutes from './routes/authRoutes';
+import cors from "cors";
+// dotenv
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// check if the database is connected
+mongoose
+  .connect(process.env.MONGO_URI || "mongodb://localhost:27017/highway-delite")
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
